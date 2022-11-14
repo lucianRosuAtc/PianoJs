c17323a8e37e7b79dd72ae3ac3667c2706332a3d
lucianRosuAtc <lucian.rosu.atc@gmail.com>
2022-11-14 13:57:21 +0000

    update
    update

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-const pianoKeys = document.querySelectorAll('.key')
 
-function playSound(newUrl){
-    new Audio(newUrl).play()
-    console.log(newUrl)
+const WHITE_KEYS = ['a', 's', 'd','f', 'g', 'h', 'j', 'k', 'l', ';', "'", '#'] 
+const BLACK_KEYS = ['w', 'e', 't', 'y', 'u', 'o', 'p', '[']
+
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+keys.forEach(key => {
+    key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e =>{
+    if(e.repeat) return
+    // if we make a repeat call for holding down the button, we call return(don't do anything )
+    const key = e.key
+    // get the key that we pressed
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key) 
+
+    if(whiteKeyIndex > -1) 
+    // -1 is returned when it cant finde anything
+    playNote(whiteKeys[whiteKeyIndex])
+    //we finding index of the key that we pressed and then corresponding that to the index in the array
+    if(blackKeyIndex > -1) playNote (blackKeys[blackKeyIndex])
+})
+
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note)
+    //dataset.note = id of the sound
+
+    noteAudio.currentTime = 0
+    //restarting the audio file at the beginning and then replaying it
+
+    noteAudio.play()
+    key.classList.add('active')
+    //the active class will change the color of the key so we know which key is pressed 
+
+    noteAudio.addEventListener('ended', ()=>{
+        key.classList.remove('active')
+    // once the .mp3 is done playing, remove the active class 
+    })
 }
-
-pianoKeys.forEach((pianoKey, i)=>{
-    const number = i < 9 ? '0'+(i+1):(i+1)
-    const newUrl = 'piano-keys/key' + number + '.mp3'
- 
-    pianoKey.addEventListener('click', () => playSound(newUrl) )
-}) 
